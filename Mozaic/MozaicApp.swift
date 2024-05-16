ec9e8b5fe583e82a1f3d81630aaad952a3d5cb3a
Max Burger <mburger89@gmail.com>
2024-05-16 10:50:43 -0700

    Initial Commit

---
+++ Mozaic/MozaicApp.swift
@@ -0,0 +1,32 @@
+//
+//  MozaicApp.swift
+//  Mozaic
+//
+//  Created by Max Burger on 5/16/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MozaicApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
